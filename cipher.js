bdabccf9ab89b436067852c350d57c2295f4999b
kcraigjones <kathryn.craigjones@gmail.com>
2021-06-11 15:15:34 -0700

    Added js file

---
+++ cipher.js
@@ -0,0 +1,24 @@
+//prints "Hello World" (test)
+//console.log('Hello World!');
+
+//if the <script src "cipher.js" doesn't work this was what was here before http://127.0.0.1:8887/firstProject/
+
+
+function rotateCipher(){
+    //store the # of rotations input in a variable "number"
+    let number = getElementByID("number").addEventListener("");
+
+//store the text input in a string called "text" (whatever I'll work on the variable names)
+
+//for loop where i = 0, i < length of text, increment i
+    //check for uppercase
+        //if character is uppercase
+            //work with ascii values 65 - 90
+            //add number of rotations to i
+        //else if character is lowercase
+            //work with ascii values 97 - 122
+            //add number of rotations to i
+        //else
+            //ignore or continue
+
+}
